0d230b3cf7b1c0bdcb6e5e45903056ba2a17e337
Riichte <rietsch.adrien@gmail.com>
2026-04-19 08:20:50 -0400

    image script final fix ?next config

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,10 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'upload.wikimedia.org' },
       { protocol: 'https', hostname: '*.unsplash.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: '*.wikimedia.org' },
+      { protocol: 'https', hostname: '*.wikipedia.org' },
     ],
+    domains: ['upload.wikimedia.org', 'commons.wikimedia.org', 'images.unsplash.com'],
   },
 };
 
